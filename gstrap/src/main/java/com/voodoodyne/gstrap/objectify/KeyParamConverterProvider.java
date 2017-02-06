ea77c726cf93550f8b76d88c852cad5e46027049
Jeff Schnitzer <jeff@infohazard.org>
2017-02-06 13:10:12 -0800

    Add KeyParamConverterProvider
    also bump version of gae and resteasy in poms

---
+++ gstrap/src/main/java/com/voodoodyne/gstrap/objectify/KeyParamConverterProvider.java
@@ -0,0 +1,43 @@
+package com.voodoodyne.gstrap.objectify;
+
+import com.googlecode.objectify.Key;
+import lombok.RequiredArgsConstructor;
+
+import javax.inject.Inject;
+import javax.ws.rs.ext.ParamConverter;
+import javax.ws.rs.ext.ParamConverterProvider;
+import javax.ws.rs.ext.Provider;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+/**
+ * Allows us to use {@code Key<?>} as JAXRS parameters, converting using the shortkey version. Since
+ * this requires some state, we need to inject the KeyStringer.
+ *
+ * To use this, bind a KeyStringer instance and register this class with Guice.
+ */
+@Provider
+@RequiredArgsConstructor(onConstructor = @__(@Inject))
+public class KeyParamConverterProvider implements ParamConverterProvider {
+
+	private final KeyStringer keyStringer;
+
+	@Override
+	public <T> ParamConverter<T> getConverter(final Class<T> aClass, final Type type, final Annotation[] annotations) {
+		if (aClass == Key.class) {
+			return new ParamConverter<T>() {
+				@Override
+				public T fromString(final String s) {
+					return aClass.cast(Key.create(keyStringer.keyify(s)));
+				}
+
+				@Override
+				public String toString(final T t) {
+					return keyStringer.stringify(((Key<?>)t).getRaw());
+				}
+			};
+		}
+
+		return null;
+	}
+}
