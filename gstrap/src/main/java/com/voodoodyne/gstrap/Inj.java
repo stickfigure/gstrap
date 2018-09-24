c02f793ea157290ac3064a7ed3dcff684cdc355f
Jeff Schnitzer <jeff@infohazard.org>
2018-09-24 10:54:57 -0700

    Add convenience method to Inj

--- gstrap/src/main/java/com/voodoodyne/gstrap/Inj.java
+++ gstrap/src/main/java/com/voodoodyne/gstrap/Inj.java
@@ -4,8 +4,10 @@
 package com.voodoodyne.gstrap;
 
 import com.google.inject.Injector;
+import com.google.inject.Key;
 
 import javax.inject.Inject;
+import java.lang.annotation.Annotation;
 
 /**
  * A static reference to the Injector so that entities and other hard-to-inject places can use
@@ -23,5 +25,9 @@ public class Inj {
 
 	static public <T> T inst(final Class<T> type) {
 		return injector.getInstance(type);
+	}
+
+	static public <T> T inst(final Class<T> type, final Class<? extends Annotation> annotationType) {
+		return injector.getInstance(Key.get(type, annotationType));
 	}
 }
